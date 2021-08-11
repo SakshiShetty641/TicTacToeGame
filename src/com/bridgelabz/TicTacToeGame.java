067124e830ada1e4dc627264cad919ac7ccbabc7
Sakshi <sakshishetty1231997@gmail.com>
2021-08-11 19:34:21 +0530

    Added: To allow the user to move to a desired location

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
  */
 
 public class TicTacToeGame {
+	static Scanner sc = new Scanner(System.in);
 	static char[] board = new char[10];
 	static char playerChoice;
 	static char computerChoice;
@@ -52,14 +53,75 @@ public class TicTacToeGame {
 		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
 	}
 
+	/**
+	 * This method allows the user to choose a desired location in the board To
+	 * select between index 1 to 9 to make the move
+	 */
+
+	static void userPlay() {
+		System.out.println("Enter board index(1-9) to set your game move ");
+		int userMove = sc.nextInt();
+		switch (userMove) {
+		case 1:
+			if (board[1] == ' ') {
+				board[1] = playerChoice;
+			}
+			break;
+		case 2:
+			if (board[2] == ' ') {
+				board[2] = playerChoice;
+			}
+			break;
+		case 3:
+			if (board[3] == ' ') {
+				board[3] = playerChoice;
+			}
+			break;
+		case 4:
+			if (board[4] == ' ') {
+				board[4] = playerChoice;
+			}
+			break;
+		case 5:
+			if (board[5] == ' ') {
+				board[5] = playerChoice;
+			}
+			break;
+		case 6:
+			if (board[6] == ' ') {
+				board[6] = playerChoice;
+			}
+			break;
+		case 7:
+			if (board[7] == ' ') {
+				board[7] = playerChoice;
+			}
+			break;
+		case 8:
+			if (board[8] == ' ') {
+				board[8] = playerChoice;
+			}
+			break;
+		case 9:
+			if (board[9] == ' ') {
+				board[9] = playerChoice;
+			}
+			break;
+		default:
+			System.out.println("Invalid Move");
+		}
+		showBoard();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Choose an option X or O");
 		char choice = sc.next().charAt(0);
 		createBoard();
 		chooseOption(choice);
 		showBoard();
+		userPlay();
 		sc.close();
 	}
 }
