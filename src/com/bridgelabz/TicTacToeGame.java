e26d48e7d308be88c766dca4667aff54ee5b8bdd
Sakshi <sakshishetty1231997@gmail.com>
2021-08-11 18:33:59 +0530

    Added: To create an empty TucTacToe board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -8,7 +8,22 @@ package com.bridgelabz;
  */
 
 public class TicTacToeGame {
+	static char[] board = new char[10];
+
+	/**
+	 * This method implements to create an empty TicTacToe board
+	 * @index This is used to initialize the index from 1 to 9
+	 * @return board This will return the board
+	 */
+	public static char[] createBoard() {
+		for (int index = 1; index < board.length; index++) {
+			board[index] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
+		createBoard();
 	}
 }
