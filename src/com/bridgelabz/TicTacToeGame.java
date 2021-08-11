336a2c4b300efd24d291a3f03f571f478f10a136
Sakshi <sakshishetty1231997@gmail.com>
2021-08-11 18:49:55 +0530

    Added: Method for the player to select X or O

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,5 +1,5 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 /**
  * Purpose - To simulate a Tic Tac Toe Game
  * @author Sakshi Shetty
@@ -9,6 +9,8 @@ package com.bridgelabz;
 
 public class TicTacToeGame {
 	static char[] board = new char[10];
+	static char playerChoice;
+	static char computerChoice;
 
 	/**
 	 * This method implements to create an empty TicTacToe board
@@ -22,8 +24,29 @@ public class TicTacToeGame {
 		return board;
 	}
 
+	/**
+	 * This method allows user to choose X or O
+	 * @return playerChoice will return option selected by the player
+	 */
+	public static char chooseOption(char choice) {
+		if (choice == 'X') {
+			playerChoice = 'X';
+			computerChoice = 'O';
+		} else if (choice == 'O') {
+			playerChoice = 'O';
+			computerChoice = 'X';
+		} else
+			System.out.println("Enter a valid Choice");
+		return playerChoice;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose an option X or O");
+		char choice = sc.next().charAt(0);
 		createBoard();
+		chooseOption(choice);
+		sc.close();
 	}
 }
